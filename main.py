13b7c82a02b6f1785093dbf1ca20fb787b18379f
Souvik Biswas <sbis1999@gmail.com>
2023-04-09 23:07:29 +0530

    Basic level structure working

--- main.py
+++ main.py
@@ -41,6 +41,46 @@ def draw_text(surface, text, size, x, y, color=(255, 255, 255), align='center'):
         text_rect.midtop = (x, y)
     surface.blit(text_surface, text_rect)
 
+def reset_game():
+    global all_sprites, aliens, player_bullets, alien_bullets, player, level, score, game_over
+
+    all_sprites.empty()
+    aliens.empty()
+    player_bullets.empty()
+    alien_bullets.empty()
+
+    player = Player(WIDTH // 2 - 32, HEIGHT - 100, screen)
+    all_sprites.add(player)
+
+    create_random_alien_grid(aliens, screen)
+
+    level = 1
+    score = 0
+    game_over = False
+
+def next_level():
+    global all_sprites, aliens, player_bullets, alien_bullets, player, level
+
+    all_sprites.empty()
+    aliens.empty()
+    player_bullets.empty()
+    alien_bullets.empty()
+
+    player = Player(WIDTH // 2 - 32, HEIGHT - 100, screen)
+    all_sprites.add(player)
+
+    create_random_alien_grid(aliens, screen)
+
+    level += 1
+    if player.lives == 3:
+        player.lives += 1
+
+def display_level_complete():
+    draw_text(screen, "Level Complete", 60, WIDTH // 2, HEIGHT // 2 - 40, color=(255, 255, 255))
+    draw_text(screen, f"Score: {score}", 40, WIDTH // 2, HEIGHT // 2 + 20, color=(255, 255, 255))
+    pygame.display.flip()
+    pygame.time.wait(3000)  # Wait for 3 seconds before proceeding to the next level
+
 # Initialize sprite groups
 all_sprites = pygame.sprite.Group()
 aliens = pygame.sprite.Group()
@@ -56,7 +96,10 @@ create_random_alien_grid(aliens, screen)
 
 alien_shoot_cooldown = 0
 score = 0
+level = 1
+
 game_over = False
+level_complete = False
 
 # Main game loop
 while True:
@@ -72,35 +115,28 @@ while True:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                if not game_over:
+                if not game_over and not level_complete:
                     bullet = Bullet(player.rect.centerx, player.rect.top, screen)
                     all_sprites.add(bullet)
                     player_bullets.add(bullet)
-                else:
+                elif game_over:
                     # Reset the game state
-                    all_sprites.empty()
-                    aliens.empty()
-                    player_bullets.empty()
-                    alien_bullets.empty()
-
-                    player.lives = 3
-                    score = 0
-                    game_over = False
-
-                    # Create a new player instance
-                    player = Player(WIDTH // 2 - 32, HEIGHT - 100, screen)
-                    all_sprites.add(player)
-
-                    # Create a new random grid of aliens
-                    create_random_alien_grid(aliens, screen)
+                    reset_game()
+                elif level_complete:
+                    level_complete = False
+                    next_level()
 
     screen.fill((0, 0, 0))
 
-    if player.lives <= 0 or len(aliens) == 0:
+    if player.lives <= 0:
         game_over = True
 
     if game_over:
         draw_text(screen, "Game Over", 60, WIDTH // 2, HEIGHT // 2, color=(255, 0, 0))
+    elif len(aliens) == 0:
+        level_complete = True
+        draw_text(screen, "Level " + str(level) + " Complete", 60, WIDTH // 2, HEIGHT // 2, color=(0, 255, 0))
+        draw_text(screen, "Press SPACE to continue", 30, WIDTH // 2, HEIGHT // 2 + 60, color=(255, 255, 255))
     else:
         # Player bullet and alien collision detection
         collisions = pygame.sprite.groupcollide(aliens, player_bullets, True, True)
@@ -128,7 +164,7 @@ while True:
         # Display score and lives
         draw_text(screen, "Score: " + str(score), 20, WIDTH - 10, 10, color=(0, 255, 0), align='right')
         draw_text(screen, "Lives: " + str(player.lives), 20, WIDTH - 10, 30, color=(0, 255, 0), align='right')
+        draw_text(screen, "Level: " + str(level), 20, 40, 10, color=(255, 255, 255), align='left')
 
     pygame.display.flip()
     clock.tick(FPS)
-
